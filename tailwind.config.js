402ae413727b1d953c16a45557b222598512a61d
Trieu Khang Trat <96287500+KayT256@users.noreply.github.com>
2024-07-06 00:06:39 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      graduate: "Graduate",
+      greatVibes: "Great Vibes",
+      mali: "Mali"
+    }
+  },
+  plugins: [],
+}
+
